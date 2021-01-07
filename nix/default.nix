78072e478edb334ebd41352f390ebc38eb0d689b
dorranh <dorran.howell@tweag.io>
2021-01-07 16:32:15 +0100

    Add shell.nix and nixpkgs

---
+++ nix/default.nix
@@ -0,0 +1,8 @@
+{ sources ? import ./sources.nix }:     # import the sources
+with
+  { overlay = _: pkgs:
+      { niv = import sources.niv {};    # use the sources :)
+      };
+  };
+import sources.nixpkgs                  # and use them again!
+  { overlays = [ overlay ] ; config = {}; }
---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import ./nix {} }:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python38
+    python38Packages.poetry
+    niv
+  ];
+}
