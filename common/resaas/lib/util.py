d62014c98192120c9b89986f4ce2762fb9731459
Simeon Carstens <simeon.carstens@tweag.io>
2021-01-19 15:26:21 +0100

    Readded files which somehow got lost

---
+++ common/resaas/lib/util.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from .storage import (FileSystemPickleStorage,
+                      FileSystemStringStorage)
+
+def log_sum_exp(x, axis=0):
+    """Calculate the log of a sum of exponentials in a numerically
+    stable way
+
+    Args:
+      x: 
+      axis:  (Default value = 0)
+
+    Returns:
+
+    """
+    xmax = x.max(axis)
+    return np.log(np.exp(x - xmax).sum(axis)) + xmax
+
+
+def storage_factory(path):
+    """Creates storage objects, which, depending on the environment, either
+    access a local file system or a cloud storage.
+
+    Args:
+      path(str): base path of a simulation
+
+    Returns:
+
+    """
+    pstorage = FileSystemPickleStorage(path)
+    sstorage = FileSystemStringStorage(path)
+    return pstorage, sstorage
