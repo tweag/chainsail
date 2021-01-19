9ffe375cb758c7fc74b8e9186676196e5e092b2d
Simeon Carstens <simeon.carstens@tweag.io>
2021-01-19 13:00:43 +0100

    Convert docstrings to google

--- lib/resaas/lib/util.py
+++ lib/resaas/lib/util.py
@@ -4,22 +4,30 @@ from .storage import (FileSystemPickleStorage,
                       FileSystemStringStorage)
 
 def log_sum_exp(x, axis=0):
-    '''
-    Calculate the log of a sum of exponentials in a numerically
+    """Calculate the log of a sum of exponentials in a numerically
     stable way
-    '''
+
+    Args:
+      x: 
+      axis:  (Default value = 0)
+
+    Returns:
+
+    """
     xmax = x.max(axis)
     return np.log(np.exp(x - xmax).sum(axis)) + xmax
 
 
 def storage_factory(path):
-    '''
-    Creates storage objects, which, depending on the environment, either
+    """Creates storage objects, which, depending on the environment, either
     access a local file system or a cloud storage.
 
-    :param path: base path of a simulation
-    :type path: str
-    '''
+    Args:
+      path(str): base path of a simulation
+
+    Returns:
+
+    """
     pstorage = FileSystemPickleStorage(path)
     sstorage = FileSystemStringStorage(path)
     return pstorage, sstorage
