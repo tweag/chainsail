5959207a29b71f29b9eb998745c633aae48c783a
Simeon Carstens <simeon.carstens@tweag.io>
2021-01-19 12:05:38 +0100

    Checkout files from re-j-b-controller

---
+++ lib/resaas/lib/util.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from .storage import (FileSystemPickleStorage,
+                      FileSystemStringStorage)
+
+def log_sum_exp(x, axis=0):
+    '''
+    Calculate the log of a sum of exponentials in a numerically
+    stable way
+    '''
+    xmax = x.max(axis)
+    return np.log(np.exp(x - xmax).sum(axis)) + xmax
+
+
+def storage_factory(path):
+    pstorage = FileSystemPickleStorage(path)
+    sstorage = FileSystemStringStorage(path)
+    return pstorage, sstorage
