835a7c8c1235c3023e2bfef92b66f2f8f007bd23
Simeon Carstens <simeon.carstens@tweag.io>
2021-01-25 23:47:39 +0100

    Fix validation bug in string storage

--- schedule_estimation/resaas/schedule_estimation/test/test_schedule_optimizers.py
+++ schedule_estimation/resaas/schedule_estimation/test/test_schedule_optimizers.py
@@ -12,17 +12,20 @@ def mock_quantity(_, __, param1, param2):
 
 class TestSingleParameterScheduleOptimizer(unittest.TestCase):
     def setUp(self):
-        self.optimizer = SingleParameterScheduleOptimizer(
-            None, None, mock_quantity, 'my_param')
+        pass
 
     def testOptimize(self):
         # TODO: I _think_ this test is okay, but for small decrements there are
         # issues, but these are, I think, related to the above
         # mock_quantity definition.
+
         for decrement in (0.1, 0.01,
                           #0.001, (fails)
                           ):
-            result = self.optimizer.optimize(0.1, 1.0, 0.1, decrement)
+            optimizer = SingleParameterScheduleOptimizer(
+                0.1, 1.0, 0.1, decrement, mock_quantity, 'my_param')
+
+            result = optimizer.optimize(dos=None, energies=None)
             expected = {'my_param': np.arange(1.0, -0.1, -0.1)}
             diffs = np.fabs(result['my_param'] - expected['my_param'])
             self.assertTrue(np.all(diffs < 1e-10))
