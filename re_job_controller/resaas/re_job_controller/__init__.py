a5220dd5b1ccaa2127e53921697005c520842f34
Simeon Carstens <simeon.carstens@tweag.io>
2021-01-25 12:10:28 +0100

    Add docstrings

--- re_job_controller/resaas/re_job_controller/__init__.py
+++ re_job_controller/resaas/re_job_controller/__init__.py
@@ -7,6 +7,7 @@ from resaas.common.util import storage_factory
 from resaas.schedule_estimation.dos_estimators import WHAM, BoltzmannEnsemble
 from resaas.schedule_estimation.schedule_optimizers import SingleParameterScheduleOptimizer
 from resaas.schedule_estimation.optimization_quantities import acceptance_rate
+from resaas.re_runners import LocalRERunner
 
 
 def log(msg):
@@ -56,70 +57,186 @@ def schedule_length(schedule):
 
 # I totally made up a word for "the one before the last one"
 def optimization_converged(preprevious_schedule, previous_schedule):
+    '''
+    Check for convergence of schedule optimization.
+
+    We say that the optimization converged if two subsequent schedules
+    have the same length. There is an off chance that parameter values
+    shift while the length stays the same, but for now that'll do.
+
+    Args:
+      preprevious_schedule(dict): a schedule obtained from an iteration
+      previous_schedule(dict): schedule obtained from the iteration after
+    '''
     return schedule_length(preprevious_schedule) \
       == schedule_length(previous_schedule)
 
 
 class InitialValueSetuper:
-    def __init__(self, pickle_storage, string_storage):
-        self.pickle_storage = pickle_storage
-        self.string_storage = string_storage
+    '''
+    Sets up an initial values for local sampling.
+    '''
+    
+    def __init__(self, pickle_storage):
+        '''
+        Initialize a schedule setuper.
+
+        Args:
+          pickle_storage(:class:`AbstractStorage`): storage backend for pickled
+                                                    Python objects
+        '''
+        self._pickle_storage = pickle_storage
 
     def _setup_timesteps(self, sim_path, schedule, previous_sim_path=None):
+        '''
+        Sets up time steps, possibly based on a previous simulation.
+
+        Args:
+          sim_path(str): path of current simulation
+          schedule(dict): current parameter schedule
+          previous_sim_path(str): path of previous simulation
+        '''
         if previous_sim_path is None:
             timesteps = np.linspace(1e-3, 1e-1, len(schedule))
         else:
-            old_timesteps = self.pickle_storage.read(
+            old_timesteps = self._pickle_storage.read(
                 previous_sim_path + FINAL_TIMESTEPS_PATH)
-            old_schedule = self.pickle_storage.read(
+            old_schedule = self._pickle_storage.read(
                 previous_sim_path + SCHEDULE_PATH)
-            timesteps = self.interpolate_timesteps(schedule, old_schedule,
-                                                   old_timesteps)
-        self.pickle_storage.write(timesteps,
-                                  sim_path + TIMESTEPS_PATH)
+            timesteps = self._interpolate_timesteps(schedule, old_schedule,
+                                                    old_timesteps)
+        self._pickle_storage.write(timesteps,
+                                   sim_path + TIMESTEPS_PATH)
 
     def _interpolate_timesteps(self, schedule, old_schedule, old_timesteps):
+        '''
+        Interpolates time steps from a previous simulation.
+
+        Given time steps from a previous simulation and its schedule,
+        this interpolates between these time steps to a new schedule
+        length.
+
+        Args:
+          schedule(dict): current parameter schedule
+          old_schedule(dict): previous parameter schedule
+          old_timesteps(dict): previous set of time steps
+        '''
         # TODO: somehow interpolate timesteps from old simulation, e.g., by
         # fitting spline or just simple linear interpolation
         return np.array([0.001] * schedule_length(schedule))
 
     def _draw_initial_states(self, schedule, previous_sim_path, dos):
-        # TODO: draw initial states using the DOS and previous states
+        '''
+        Draw initial states using the density of states.
+
+        Given previous samples, their energies, and the current parameter
+        schedule, this uses the density of states to calculate probability
+        weights of the previous samples under the new schedule and uses
+        those weights to sample fitting new initial states from the existing
+        samples.
+
+        Args:
+          schedule(dict): current temperature schedule
+          previous_sim_path(dict): path of previous simulation
+          dos(:class:`np.array`): density of states estimate of previous
+              simulation
+        '''
+        # TODO
         return np.array([0.001] * schedule_length(schedule))
 
     def setup_initial_states(self, sim_path, schedule, dos=None,
                              previous_sim_path=None):
+        '''
+        Sets up initial states.
+
+        Uses the density of states to draw new initial states and writes them
+        to the simulation directory.
+
+        Args:
+          sim_path(str): path of current simulation
+          schedule(dict): current paramter schedule
+          dos(:class:`np.array`): estimate of the density of states
+          previous_sim_path(str): path of previous simulation
+        '''
         if previous_sim_path is not None:
             initial_states = self._draw_initial_states(schedule,
                                                        previous_sim_path,
                                                        dos)
-            self.pickle_storage.write(initial_states,
-                                      sim_path + INITIAL_STATES_PATH)
+            self._pickle_storage.write(initial_states,
+                                       sim_path + INITIAL_STATES_PATH)
 
 
 class AbstractInitialScheduleMaker(ABC):
+    '''
+    Interface for classes which are responsible for creating
+    initial schedules.
+    '''
+
     def __init__(self, param_name):
         self._param_name = param_name
 
     @abstractmethod
     def make_initial_schedule(self, **params):
+        '''
+        Make an initial schedule from some parameters.
+        '''
         pass
 
 
 class GeometricInitialScheduleMaker(AbstractInitialScheduleMaker):
+    '''
+    Schedule maker which makes a schedule using a geometric progression.
+    '''
+
     def make_initial_schedule(self, num_replicas, min_value, max_value=1.0):
+        '''
+        Makes a schedule of replica parameters (usually inverse temperatures)
+        with a fixed length defined by a geometric progression.
+
+        Args:
+          num_replicas(int): number of replicas
+          min_value(float): minimum parameter value
+          max_value(float): maximum parameter value
+        '''
         common_ratio = (max_value / min_value) ** (1. / num_replicas)
         geometric_progression = common_ratio ** np.arange(num_replicas)
         return {self._param_name: geometric_progression}
 
 
 class AbstractREJobController(ABC):
+    '''
+    Interface for Replica Exchange job controllers. They implement the main
+    loop of running a simulation, optimizing the schedule, determining new
+    initial states for the next simulation, setting it up and running it.
+    '''
+
     def __init__(self, job_spec, re_runner,
                  initial_schedule_maker=GeometricInitialScheduleMaker,
                  schedule_optimizer=SingleParameterScheduleOptimizer,
                  ensemble=BoltzmannEnsemble,
                  optimization_quantity=acceptance_rate,
                  dos_estimator=WHAM):
+        '''
+        Initializes a Replixa Exchange job controller.
+
+        Arguments contain everything required for running simulations and
+        optimizing schedules.
+
+        Args:
+          job_spec(dict): job specifications provided by the user
+          initial_schedule_maker(:class:`InitialScheduleMaker`): schedule
+              maker objects which calculates a very first Replica Exchange
+              schedule
+          schedule_optimizer(:class:`AbstractScheduleOptimizer`): schedule
+              optimizer class which calculates a new schedule based on
+              an estimate for the density of states
+          ensemble(:class:`AbstractEnsemble`): the ensemble (family of
+              tempered distributions) to use
+          optimization_quantity(callable): the quantitiy which controls
+              schedule optimization
+          dos_estimator(:class:`WHAM`): WHAM implementation which estimates the
+              density of states from samples of a previous simulation
+        '''
         self._re_runner = re_runner
         self._initial_schedule_maker = initial_schedule_maker
         self._schedule_optimizer = schedule_optimizer
@@ -136,6 +253,12 @@ class AbstractREJobController(ABC):
         self._set_params(job_spec)
 
     def _set_params(self, job_spec):
+        '''
+        Set different parameter dictionaries from job specification.
+
+        Args:
+          job_spec(dict): job specification
+        '''
         self._re_params = job_spec['re_params']
         self._local_sampling_params = job_spec['local_sampling_params']
         self._optimization_params = job_spec['optimization_params']
@@ -143,10 +266,27 @@ class AbstractREJobController(ABC):
 
     @abstractmethod
     def _scale_environment(self, num_replicas):
+        '''
+        Scale up / down the environment to the given numer of replicas.
+
+        Args:
+          num_replicas(int): number of replicas
+        '''
         pass
 
     def optimize_schedule(self):
-        run_counter = 0
+        '''
+        Run schedule optimization loop.
+
+        This is the main loop in this class: first, an initial schedule is
+        calculated and a very first simulation is run. Its samples are then
+        used to calculate an estimate of the density of states, which is then
+        used to calculate the schedule and initial states for the next
+        optimization run..
+        Its results are then used to improve the schedule in another run and
+        so on and so forth.
+        '''
+        run_counter = 1
         dos = None
         previous_schedule = None
         preprevious_schedule = None
@@ -166,7 +306,13 @@ class AbstractREJobController(ABC):
                     self._optimization_params['target_value'],
                     self._optimization_params['max_param'],
                     self._optimization_params['min_param'],
-                    self._optimization_params['decrement'],)
+                    self._optimization_params['decrement'])
+                if run_counter == opt_params['max_optimization_runs']:
+                    submsg = 'final run'
+                else:
+                    submsg = 'optimization run #{}'.format(run_counter)
+                log('Calculated schedule for {} '.format(submsg)
+                    + 'with {} replicas'.format(schedule_length(schedule)))
             else:
                 schedule = self._initial_schedule_maker.make_initial_schedule(
                     **self._initial_schedule_params)
@@ -188,6 +334,15 @@ class AbstractREJobController(ABC):
 
     def _fill_config_template(self, sim_path, previous_sim_path, schedule,
                               prod=False):
+        '''
+        Fills in the config template with run-specific values.
+
+        Args:
+          sim_path(str): path of the current simulation
+          previous_sim_path(str): path of the previous simulation
+          schedule(dict): schedule of the current simulation
+          prod(bool): whether this is the production run or not
+        '''
         updates = {'local_sampling': {}, 'general': {}, }
         adapt_limit = self._local_sampling_params['hmc_num_adaption_steps']
         updates['local_sampling']['timestep_adaption_limit'] = adapt_limit
@@ -210,6 +365,22 @@ class AbstractREJobController(ABC):
 
     def _setup_simulation(self, sim_path, schedule, previous_dos=None,
                           previous_sim_path=None, prod=False):
+        '''
+        Sets up a simulation such that a RE runner only has to start it.
+
+        Setting up a simulation involves determining initial values for local
+        sampling, writing them to the simulation folder, completing the
+        simulation config and using it to write a config file to the simulation
+        folder.
+
+        Args:
+          sim_path(str): path of the simulation
+          schedule(dict): Replica Exchange schedule
+          previous_dos(:class:`np.ndarray`): density of states estimate obtained
+              from the previous simulation
+          previous_sim_path(str): path of previous simulation
+          prod(bool): whether this is the production run or not
+        '''
         if previous_sim_path is None:
             self._initial_value_setuper.setup_timesteps(sim_path, schedule,
                                                         previous_sim_path)
@@ -217,13 +388,19 @@ class AbstractREJobController(ABC):
                 sim_path, schedule, previous_dos, previous_sim_path)
 
         config_dict = self._fill_config_template(sim_path, schedule, prod)
-        config = self._make_config_str(**config_dict)
+        config = yaml.dump(config_dict)
         self._string_storage.write(config, sim_path + CONFIG_PATH,
                                    mode_flags='w')
         self._pickle_storage.write(schedule, sim_path + SCHEDULE_PATH)
         self._scale_environment(schedule_length(schedule))
 
     def _load_energies(self, sim_path):
+        '''
+        Loads energies from a simulation.
+
+        Args:
+          sim_path(str): path of the simulation
+        '''
         config = yaml.safe_load(self._string_storage.read(sim_path + CONFIG_PATH))
         n_replicas = config['general']['num_replicas']
         n_samples = config['general']['n_iterations']
@@ -240,6 +417,16 @@ class AbstractREJobController(ABC):
         return np.array(energies)
 
     def _do_single_run(self, sim_path):
+        '''
+        Run a single Replica Exchange simulation, estimate the density of
+        states and write it to the simulation folder.
+
+        Args:
+          sim_path(str): path of the simulation
+
+        Returns:
+          :class:`np.array`: density of states estimate
+        '''
         self._re_runner.run_sampling(sim_path)
         energies = self._load_energies(sim_path)
         schedule = self._pickle_storage.read(sim_path + SCHEDULE_PATH)
@@ -250,18 +437,25 @@ class AbstractREJobController(ABC):
 
     def _check_compatibility(self, initial_schedule_params,
                              optimization_params):
+        '''
+        Check whether initial schedule parameters are compatible with the
+        optimization parameters.
+
+        Args:
+          initial_schedule_params(dict): initial schedule parameters
+          optimization_params(dict): optimization params
+        '''
         # TODO: check whether optimization params and initial schedule params
         # are compatible. Although that's a late failure - perhaps do this upon
         # job submission?
         pass
 
-    def _make_config_str(self, **kwargs):
-        cfg = cfg_template.copy()
-        for key, value in kwargs.items():
-            cfg[key].update(**value)
-        return yaml.dump(cfg)
-
     def run_job(self):
+        '''
+        Runs a complete Replica Exchange sampling job.
+
+        This comprises schedule optimization and a final production run.
+        '''
         self._check_compatibility(self._initial_schedule_params,
                                   self._optimization_params)
 
@@ -274,16 +468,21 @@ class AbstractREJobController(ABC):
 
 
 class LocalREJobController(AbstractREJobController):
+    '''
+    Local Replica Exchange job controller, which does not scale up / down
+    the environment.
+    '''
+
     def _scale_environment(self, _):
         pass
 
 
-job_spec = dict(re_params=dict(num_replicas=8, num_optimization_samples=5000),
-                local_sampling_params=dict(hmc_num_adaption_steps=100),
-                optimization_params=dict(target_value=0.3, max_param=1,
-                                         min_param=0.1, decrement=0.05),
-                initial_schedule_params=dict(beta_min=0.01, beta_ratio=0.9),
-                path='/tmp/jctest/')
+# job_spec = dict(re_params=dict(num_replicas=8, num_optimization_samples=5000),
+#                 local_sampling_params=dict(hmc_num_adaption_steps=100),
+#                 optimization_params=dict(target_value=0.3, max_param=1,
+#                                          min_param=0.1, decrement=0.05),
+#                 initial_schedule_params=dict(beta_min=0.01, beta_ratio=0.9),
+#                 path='/tmp/jctest/')
 
-c = REJobController(job_spec, MockRERunner())
-c.run_job()
+# c = LocalREJobController(job_spec, MockRERunner())
+# c.run_job()
