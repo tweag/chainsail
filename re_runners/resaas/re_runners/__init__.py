0f0c2b4634689cf34bc9236997e45c96cc20fa69
Simeon Carstens <simeon.carstens@tweag.io>
2021-01-27 12:32:07 +0100

    Use refactored storage

--- re_runners/resaas/re_runners/__init__.py
+++ re_runners/resaas/re_runners/__init__.py
@@ -5,14 +5,6 @@ from abc import abstractmethod, ABC
 import os
 from subprocess import check_output
 
-import yaml
-
-# TODO: make adding `resaas.common` as a path dependency work
-import sys
-sys.path.append(os.getenv('RESAAS_COMMON_PATH'))
-
-from resaas.common.util import storage_factory
-
 
 def get_run_script_path():
     return check_output(['which', 'run_simulation']).strip()
@@ -20,17 +12,16 @@ def get_run_script_path():
 
 class AbstractRERunner(ABC):
     @abstractmethod
-    def run_sampling(self, config_file, basename):
+    def run_sampling(self, storage):
         pass
 
 
 class MPIRERunner(AbstractRERunner):
-    def run_sampling(self, config_file, basename):
-        _, string_storage = storage_factory(basename)
-        config = yaml.load(string_storage.read(config_file))
+    def run_sampling(self, storage):
+        config = storage.load_config_file()
         n_replicas = config['general']['num_replicas']
-        cfg_path = config['general']['output_path'] + 'config.yml'
-        cfg_path = basename + cfg_path
+        cfg_path = os.path.join(storage.basename, storage.sim_path,
+                                storage.dir_structure.CONFIG_FILE_NAME)
         mpirun_args = ['--oversubscribe', '-n', str(n_replicas + 1)]
         run_script_args = [cfg_path, "local"]
         output = check_output(['mpirun'] + mpirun_args +
