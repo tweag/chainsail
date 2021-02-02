202c352e754536c706746cbc219476acc911f9f6
Simeon Carstens <simeon.carstens@tweag.io>
2021-02-02 15:58:30 +0100

    Add check for zero parameter values

--- re_job_controller/resaas/re_job_controller/initial_schedules.py
+++ re_job_controller/resaas/re_job_controller/initial_schedules.py
@@ -8,17 +8,25 @@ import numpy as np
 def make_geometric_schedule(param_name, num_replicas, min_value,
                             max_value=1.0):
     '''
-    Schedule maker which makes a schedule using a geometric progression.
+    Schedule maker which makes a schedule for an inverse-temperature-like
+    parameter using a geometric progression.
 
     In fact it creates a geometric progression in the temperatures,
     but which are then converted into _inverse_ temperatures.
 
     Args:
         param_name(str): name of replica parameter
-        min_value(float): minimum parameter value
-        max_value(float): maximum parameter value
+        min_value(float): minimum parameter value; must be greater than zero
+        max_value(float): maximum parameter value; must be greater than zero
         num_replicas(int): number of replicas
     '''
+    if min_value <= 0:
+        raise ValueError("Minimum parameter value has to be greater than zero")
+    if max_value <= 0:
+        raise ValueError("Maximum parameter value has to be greater than zero")
+    if max_value <= min_value:
+        raise ValueError(("Maximum paramter value has to be "
+                          "greater than minmum parameter value"))
     max_temp = 1 / min_value
     min_temp = 1 / max_value
     common_ratio = (max_temp / min_temp) ** (1 / (num_replicas - 1))
