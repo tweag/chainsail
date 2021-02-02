ee38e82faf47a42b843c0de1afae9fbdc9f9c4e9
dorranh <dorran.howell@tweag.io>
2021-02-02 08:27:52 +0100

    Update dockerfile

--- docker/node/base/entrypoint.sh
+++ docker/node/base/entrypoint.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
-# TODO: The scheduler needs to handle this 
-
-set -ex
+set -e
 
 mkdir -p /root/.ssh
-cp /app/config/ssh/authorized_keys /root/.ssh/authorized_keys
-chown root /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
+
+# Copy over ssh public key(s) for ssh server and adjust permissions
+if [ -f /app/config/ssh/authorized_keys ]; then
+    echo "Found ssh keys for server. Installing."
+    cp /app/config/ssh/authorized_keys /root/.ssh/authorized_keys
+    chown root /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
+else
+    echo "No ssh public keys found for server."
+fi
+
+# if [ -z "$USER_DATA_URL" ]
+# then
+#       echo 'USER_DATA_URL not specified. Not fetching any data.'
+# else
+#       echo "Downloading user data from $USER_DATA_URL"
+#       curl -L 
+# fi
 
 exec "$@"
