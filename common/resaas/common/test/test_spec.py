187b6faa2e072c7d9e16018e57cf2f9f5211f0cf
Dorran Howell <52341904+dorranh@users.noreply.github.com>
2021-02-02 15:09:30 +0100

    Fix bug in job spec serialization (#68)
    Updates JobSpecSchema serialization to properly handle round-trip serialization/deserialization and added a test to ensure that all data is preserved when re-loading serialized data.

--- common/resaas/common/test/test_spec.py
+++ common/resaas/common/test/test_spec.py
@@ -1,11 +1,45 @@
 import pytest
-
 from marshmallow.exceptions import ValidationError
+
+
+def test_pip_deps_eq():
+    from resaas.common.spec import PipDependencies
+
+    dep_1 = PipDependencies(["numpy>1.15", "scipy"])
+    dep_2 = PipDependencies(["numpy>1.15", "scipy"])
+
+    assert dep_1 == dep_2
+
+
+def test_pip_deps_not_eq():
+    from resaas.common.spec import PipDependencies
+
+    dep_1 = PipDependencies(["tensorflow", "scipy"])
+    dep_2 = PipDependencies(["numpy>1.15", "scipy"])
+
+    assert not (dep_1 == dep_2)
+
+
+def test_spec_eq():
+    from resaas.common.spec import JobSpec, PipDependencies
+
+    spec_1 = JobSpec("foo/bar")
+    spec_2 = JobSpec("foo/bar")
+
+    assert spec_1 == spec_2
+
+
+def test_spec_not_eq():
+    from resaas.common.spec import JobSpec, PipDependencies
+
+    spec_1 = JobSpec("foo/bar")
+    spec_2 = JobSpec("other")
+
+    assert not (spec_1 == spec_2)
 
 
 def test_parse_job_spec_extra_fields():
     from marshmallow.exceptions import ValidationError
-
     from resaas.common.spec import JobSpecSchema
 
     data = """
@@ -40,6 +74,24 @@ def test_parse_partial_job_spec():
     JobSpecSchema().loads(data)
 
 
+def test_parse_job_spec_empty_dependencies():
+    from resaas.common.spec import JobSpecSchema
+
+    data = """
+    {
+        "probability_definition": "gs://bucket/sub/path/script_and_data",
+        "max_replicas": 2,
+        "initial_schedule_parameters": {
+            "minimum_beta": 1
+        },
+        "dependencies": []
+    }
+    """
+    schema = JobSpecSchema()
+    spec = schema.loads(data)
+    assert spec.dependencies == []
+
+
 def test_parse_wrong_initial_schedule_params_job_spec():
     from resaas.common.spec import JobSpecSchema
 
@@ -53,3 +105,28 @@ def test_parse_wrong_initial_schedule_params_job_spec():
     """
     with pytest.raises(ValidationError):
         JobSpecSchema().loads(data)
+
+
+def test_job_spec_serialization_round_trip():
+
+    from resaas.common.spec import JobSpecSchema
+
+    data = """
+    {
+        "probability_definition": "gs://bucket/sub/path/script_and_data",
+        "max_replicas": 2,
+        "initial_schedule_parameters": {
+            "minimum_beta": 1
+        },
+        "dependencies": [
+            {
+                "type": "pip",
+                "deps": ["numpy==1.19.5", "pandas==1.0.0"]
+            }
+        ]
+    }
+    """
+    schema = JobSpecSchema()
+    spec_1 = schema.loads(data)
+    spec_2 = schema.loads(schema.dumps(spec_1))
+    assert spec_1 == spec_2
