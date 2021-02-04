92c389acf5854e6897493aa14a79bd9c8b459355
Simeon Carstens <simeon.carstens@tweag.io>
2021-02-04 23:49:04 +0100

    Forgot to git add the test

--- re_job_controller/resaas/re_job_controller/test/test_initial_setup.py
+++ re_job_controller/resaas/re_job_controller/test/test_initial_setup.py
@@ -3,8 +3,12 @@ import unittest
 import numpy as np
 
 from resaas.schedule_estimation.dos_estimators import WHAM, BoltzmannEnsemble
+<<<<<<< HEAD
 from resaas.re_job_controller.initial_setup import (draw_initial_states,
                                                     interpolate_timesteps)
+=======
+from resaas.re_job_controller.initial_setup import draw_initial_states
+>>>>>>> 829c754... Forgot to git add the test
 
 
 class MockStorage:
