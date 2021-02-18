0e42350f3a10b94b3b3f8e2f2824c4839e7748a0
Simeon Carstens <simeon.carstens@tweag.io>
2021-02-18 11:19:30 +0100

    Small refactoring

--- examples/use-cases/or-fitting/probability.py
+++ examples/use-cases/or-fitting/probability.py
@@ -7,6 +7,9 @@ def line(a, b, x):
     return a * x + b
 
 
+def line_gradient(a, b, x): return np.array((x, np.ones(len(x))))
+
+
 def rn_average(r, n, axis):
     return np.sum(np.array(r) ** (-n), axis=axis) ** (-1 / n)
 
@@ -44,10 +47,10 @@ class Posterior:
     def log_likelihood_gradient(self, x):
         mock_y = line(x[0], x[1], self.data_x)
         r = self.data_y - mock_y[:, None]
-        num_data = len(self.data_x)
-        line_gradient = np.array((self.data_x, np.ones(num_data)))
+        line_grad = line_gradient(x[0], x[1], self.data_x)
         num_ys = self.data_y.shape[1]
-        abs_grad = (-r / np.fabs(r))[..., None] * np.tile(line_gradient.T, num_ys).reshape(-1, num_ys, 2)
+        abs_grad = (-r / np.fabs(r))[..., None] * np.tile(
+            line_grad.T, num_ys).reshape(-1, num_ys, 2)
         outermost = -rn_average(np.fabs(r), self.n, axis=1)
         avg_grad = rn_average_grad(np.fabs(r), self.n, axis=1)
         inner = np.sum(avg_grad[..., None] * abs_grad, 1)
