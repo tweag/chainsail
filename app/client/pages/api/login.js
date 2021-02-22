d41ba0a0755862f0f289be12c5d9dfe9f7dbc882
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-22 16:30:35 +0100

    Add error to login/logout log message

--- app/client/pages/api/login.js
+++ app/client/pages/api/login.js
@@ -7,7 +7,7 @@ const handler = async (req, res) => {
   try {
     await setAuthCookies(req, res);
   } catch (e) {
-    return res.status(500).json({ error: 'Unexpected error.' });
+    return res.status(500).json({ error: e });
   }
   return res.status(200).json({ success: true });
 };
