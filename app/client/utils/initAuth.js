9e02ee63428b8ae36742e8479bb18f366cb2438f
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-22 15:33:49 +0100

    intermediate modified:   initAuth.js

--- app/client/utils/initAuth.js
+++ app/client/utils/initAuth.js
@@ -1,38 +1,42 @@
 import { init } from 'next-firebase-auth';
 
+const TWELVE_DAYS_IN_MS = 12 * 60 * 60 * 24 * 1000;
+
 const initAuth = () => {
   init({
-    authPageURL: '/auth',
+    debug: false,
+    authPageURL: '/login',
     appPageURL: '/',
-    loginAPIEndpoint: '/api/login', // required
-    logoutAPIEndpoint: '/api/logout', // required
-    // Required in most cases.
+    loginAPIEndpoint: '/api/login',
+    logoutAPIEndpoint: '/api/logout',
     firebaseAdminInitConfig: {
       credential: {
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // The private key must not be accesssible on the client side.
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        // Using JSON to handle newline problems when storing the
+        // key as a secret in Vercel. See:
+        // https://github.com/vercel/vercel/issues/749#issuecomment-707515089
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+          ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+          : undefined,
       },
       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     },
     firebaseClientInitConfig: {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY, // required
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
       authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     },
     cookies: {
-      name: 'Resaas', // required
-      // Keys are required unless you set `signed` to `false`.
-      // The keys cannot be accessible on the client side.
+      name: 'ExampleApp',
       keys: [process.env.COOKIE_SECRET_CURRENT, process.env.COOKIE_SECRET_PREVIOUS],
       httpOnly: true,
-      maxAge: 12 * 60 * 60 * 24 * 1000, // twelve days
+      maxAge: TWELVE_DAYS_IN_MS,
       overwrite: true,
       path: '/',
       sameSite: 'strict',
-      secure: true, // set this to false in local (non-HTTPS) development
+      secure: process.env.NEXT_PUBLIC_COOKIE_SECURE === 'true',
       signed: true,
     },
   });
