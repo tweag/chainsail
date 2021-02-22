af52672e9ea0cd53ea56a5779d8f923db2b44e46
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-22 12:12:27 +0100

    Add initAuth to _app.js

--- app/client/pages/_app.js
+++ app/client/pages/_app.js
@@ -1,6 +1,9 @@
 import 'tailwindcss/tailwind.css';
 import 'katex/dist/katex.min.css';
 import '../style/global.css';
+import initAuth from '../utils/initAuth';
+
+initAuth();
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
