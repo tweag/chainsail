58336494547aebd046d90fbe8da68e8432e9cb82
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-22 13:24:32 +0100

    Add Google auth to Firebase ui config

--- app/client/components/FirebaseAuth.js
+++ app/client/components/FirebaseAuth.js
@@ -10,12 +10,7 @@ const firebaseAuthConfig = {
   signInFlow: 'popup',
   // Auth providers
   // https://github.com/firebase/firebaseui-web#configure-oauth-providers
-  signInOptions: [
-    {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      requireDisplayName: false,
-    },
-  ],
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   signInSuccessUrl: '/',
   credentialHelper: 'none',
   callbacks: {
