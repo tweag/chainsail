701524f13d14c2f6a4923dd0ceeb01bbb152f226
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-22 13:46:09 +0100

    modified:   auth.js

--- app/client/pages/auth.js
+++ app/client/pages/auth.js
@@ -1,34 +1,23 @@
 import { withAuthUser, AuthAction } from 'next-firebase-auth';
-import FirebaseAuth from '../components/FirebaseAuth';
+import { FlexCenter, FirebaseAuth, FlexCol } from '../components';
 
-const styles = {
-  content: {
-    padding: `8px 32px`,
-  },
-  textContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    margin: 16,
-  },
-};
-
-const Auth = () => (
-  <div style={styles.content}>
-    <h3>Sign in</h3>
-    <div style={styles.textContainer}>
-      <p>
-        This auth page is <b>static</b>. It will redirect on the client side if the user is already
-        authenticated.
-      </p>
-    </div>
-    <div>
+const Login = () => (
+  <FlexCenter className="h-screen w-full">
+    <FlexCol>
+      <h3>Sign in</h3>
+      <div>
+        <p>
+          This auth page is <b>static</b>. It will redirect on the client side if the user is
+          already authenticated.
+        </p>
+      </div>
       <FirebaseAuth />
-    </div>
-  </div>
+    </FlexCol>
+  </FlexCenter>
 );
 
 export default withAuthUser({
   whenAuthed: AuthAction.REDIRECT_TO_APP,
   whenUnauthedBeforeInit: AuthAction.RETURN_NULL,
   whenUnauthedAfterInit: AuthAction.RENDER,
-})(Auth);
+})(Login);
