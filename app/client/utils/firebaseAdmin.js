1b066c6a929af3abb8ecde6842a72e688045304a
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-23 12:00:51 +0100

    Keep secrets in local files

--- app/client/utils/firebaseAdmin.js
+++ app/client/utils/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase-admin-secrets.json');
+const serviceAccount = require('../firebase-admin-secrets.local.json');
 
 export const verifyIdToken = (token) => {
   if (!admin.apps.length) {
