225dca867679e89709f9d5d4940b49651045063b
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-23 13:32:13 +0100

    Cleanup Navbar

--- app/client/components/Navbar.js
+++ app/client/components/Navbar.js
@@ -1,29 +1,25 @@
-import { FlexRow } from './Flex';
 import Link from 'next/link';
 import { useState } from 'react';
+
+import { FlexRow } from './Flex';
 
 const NavItem = (props) => {
   return (
     <Link href={props.href}>
-      <a
-        className={`mx-2 py-2 px-4 border-white border-opacity-20 ${
-          props.isActive ? 'border-b-2' : 'border-b-0'
-        } hover:opacity-60 transition duration-300`}
+      <div
+        className="mx-2 py-2 px-4 border-white border-opacity-20 hover:opacity-60 transition duration-300"
         onClick={props.onClick}
       >
         {props.children}
-      </a>
+      </div>
     </Link>
   );
 };
 
 const Navbar = () => {
-  const [activeItem, setAcitveItem] = useState('home');
   return (
     <FlexRow className="items-center h-16 text-sm md:text-base">
-      <NavItem href="/" isActive={activeItem === 'home'} onClick={() => setAcitveItem('home')}>
-        Home
-      </NavItem>
+      <NavItem href="/">Home</NavItem>
     </FlexRow>
   );
 };
