919651815bc6360b1c94a4545a3f243fe32a7f8f
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-23 11:20:45 +0100

    new file:   utils/firebaseAdmin.js

---
+++ app/client/utils/firebaseAdmin.js
@@ -0,0 +1,18 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('../secret.json');
+
+export const verifyIdToken = (token) => {
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: 'https://nextjs-firebase-auth-9bc98.firebaseio.com',
+    });
+  }
+
+  return admin
+    .auth()
+    .verifyIdToken(token)
+    .catch((error) => {
+      throw error;
+    });
+};
