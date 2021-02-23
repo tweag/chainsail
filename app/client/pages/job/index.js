a7aaf131e0a0c16e35c3823cfe72e152456581ca
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-23 22:11:53 +0100

    Set job form padding to zero

--- app/client/pages/job/index.js
+++ app/client/pages/job/index.js
@@ -236,7 +236,7 @@ export default function Job() {
             contains the samples, sampling statistics such as acceptance rates and an estimate of
             the density of states.
           </div>
-          <FlexRow between responsive media="lg" className="w-full lg:px-20 lg:h-4/5 lg:space-x-20">
+          <FlexRow between responsive media="lg" className="w-full lg:h-4/5 lg:space-x-20">
             <FlexCenter className="flex-grow mb-10 lg:py-10 h-96 md:h-80 lg:h-full lg:mb-0 w-96">
               <form
                 className="h-full"
