266d673be0508eded5a48554b5f9c768a642f7ab
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-23 11:47:44 +0100

    Add AuthProvider wrapper to _app.js

--- app/client/pages/_app.js
+++ app/client/pages/_app.js
@@ -2,8 +2,14 @@ import 'tailwindcss/tailwind.css';
 import 'katex/dist/katex.min.css';
 import '../style/global.css';
 
+import { AuthProvider } from '../components/Auth';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
