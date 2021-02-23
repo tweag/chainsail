3e5e553d31f951b16362218c5f38d75f87830703
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-23 13:52:23 +0100

    Add login/logout to Navbar

--- app/client/components/Navbar.js
+++ app/client/components/Navbar.js
@@ -1,27 +1,54 @@
 import Link from 'next/link';
+import firebase from 'firebase/app';
 
 import { FlexRow } from './Flex';
 import { useAuth } from './Auth';
+import firebaseClient from '../utils/firebaseClient';
 
 const NavItem = (props) => {
-  return (
-    <Link href={props.href}>
-      <div
-        className="mx-2 py-2 px-4 border-white border-opacity-20 hover:opacity-60 transition duration-300"
-        onClick={props.onClick}
-      >
+  const style =
+    'px-4 py-2 mx-2 border-white border-opacity-20 hover:opacity-60 transition duration-300 cursor-pointer';
+  if (props.internal) {
+    return (
+      <Link href={props.href}>
+        <a className={style}>{props.children}</a>
+      </Link>
+    );
+  }
+  if (props.href) {
+    return (
+      <a className={style} href={props.href}>
         {props.children}
-      </div>
-    </Link>
-  );
+      </a>
+    );
+  }
+  if (props.onClick) {
+    return (
+      <a className={style} onClick={props.onClick}>
+        {props.children}
+      </a>
+    );
+  }
+};
+
+const logout = async () => {
+  await firebase.auth().signOut();
+  window.location.href = '/';
 };
 
 const Navbar = () => {
-  const user = useAuth();
+  const { user } = useAuth();
   return (
     <FlexRow className="items-center h-16 text-sm md:text-base">
-      <NavItem href="/">Home</NavItem>
-      {!user && <NavItem href="/login">Login</NavItem>}
+      <NavItem internal href="/">
+        Home
+      </NavItem>
+      {!user && (
+        <NavItem internal href="/login">
+          Login
+        </NavItem>
+      )}
+      {user && <NavItem onClick={logout}>Logout</NavItem>}
     </FlexRow>
   );
 };
