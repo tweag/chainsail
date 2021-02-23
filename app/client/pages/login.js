cc678ba371598273cb209e702c8aba799291510f
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-23 11:53:33 +0100

    new file:   pages/login.js

---
+++ app/client/pages/login.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import firebaseClient from '../utils/firebaseClient';
+
+const firebaseAuthConfig = {
+  signInFlow: 'popup',
+  // Auth providers
+  // https://github.com/firebase/firebaseui-web#configure-oauth-providers
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  signInSuccessUrl: '/',
+  credentialHelper: 'none',
+  callbacks: {
+    signInSuccessWithAuthResult: () => false,
+  },
+};
+
+const FirebaseAuth = () => {
+  firebaseClient();
+  const [renderAuth, setRenderAuth] = useState(false);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setRenderAuth(true);
+    }
+  }, []);
+  return (
+    <div>
+      {renderAuth ? (
+        <StyledFirebaseAuth uiConfig={firebaseAuthConfig} firebaseAuth={firebase.auth()} />
+      ) : null}
+    </div>
+  );
+};
+
+export default FirebaseAuth;
