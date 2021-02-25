19fb407fad94adc0204f20a31d854fbff737424e
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-25 14:36:50 +0100

    Make uuid job spec optional

--- lib/common/resaas/common/spec.py
+++ lib/common/resaas/common/spec.py
@@ -233,7 +233,7 @@ INITIAL_SCHEDULE_PARAMETERS_SCHEMAS = {
 
 class JobSpecSchema(Schema):
     probability_definition = fields.String(required=True)
-    uuid = fields.String(required=True)
+    uuid = fields.String(required=False)
     name = fields.String(required=False)
     initial_number_of_replicas = fields.Int()
     initial_schedule_parameters = fields.Dict(fields.String, fields.Float())
@@ -296,7 +296,7 @@ class JobSpec:
     def __init__(
         self,
         probability_definition: str,
-        uuid: str,
+        uuid: Optional[str] = None,
         name: Optional[str] = None,
         initial_number_of_replicas: int = 10,
         initial_schedule_parameters: Optional[
