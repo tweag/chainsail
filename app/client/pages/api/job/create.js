26e6ef4e9fad9bd1a9192c537642bdc27d952c7f
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-02-25 20:15:18 +0100

    intermediate

--- app/client/pages/api/job/create.js
+++ app/client/pages/api/job/create.js
@@ -3,13 +3,14 @@ import { FLASK_URL, JOB_CREATION_ENDPOINT } from '../../../utils/const';
 export default async (req, res) => {
   const { token } = req.cookies;
   const body = JSON.stringify(req.body);
+  const endpoint = JOB_CREATION_ENDPOINT;
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
     body,
   };
   try {
-    const response = await fetch(`${FLASK_URL}${JOB_CREATION_ENDPOINT}`, requestOptions);
+    const response = await fetch(`${FLASK_URL}${endpoint}`, requestOptions);
     const res_body = await response.json();
     res.status(response.status).json(res_body);
   } catch (e) {
