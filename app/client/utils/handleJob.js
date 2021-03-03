d7c48ae13f9ad6bf3d2bf80c5403bbf8cf950c43
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-03-03 18:21:46 +0100

    Remove getjob from utils modified:   utils/handleJob.js

--- app/client/utils/handleJob.js
+++ app/client/utils/handleJob.js
@@ -17,11 +17,3 @@ export const stopJob = (jobId) => {
   };
   fetch('/api/job/stop', requestOptions);
 };
-
-export const getJob = (jobId) => {
-  const requestOptions = {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-  };
-  return fetch(`/api/job/get/${jobId}`, requestOptions);
-};
