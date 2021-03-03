698fa09839ceebfc617cec7f7c9bbce7cad15d93
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-03-03 18:08:49 +0100

    renamed:    get.js -> [jobId].js

--- app/client/pages/api/job/get/[jobId].js
+++ app/client/pages/api/job/get/[jobId].js
@@ -1,10 +1,8 @@
-import { useRouter } from 'next/router';
 import { FLASK_URL, JOB_GET_ENDPOINT } from '../../../utils/const';
 import handleRequestResponse from '../../../utils/handleRequestResponse';
 
 export default async (req, res) => {
-  const router = useRouter();
-  const { jobId } = router.query;
+  const { jobId } = req.query;
   const url = `${FLASK_URL}${JOB_GET_ENDPOINT(jobId)}`;
   const method = 'GET';
   handleRequestResponse(req, res, url, method);
