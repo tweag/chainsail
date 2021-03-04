10bd4a5d8fb3992b6710bbf0b2d5ca80809f77d5
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-03-04 11:16:11 +0100

    Use JobButton component in result page

--- app/client/pages/job/results.js
+++ app/client/pages/job/results.js
@@ -2,45 +2,8 @@ import useSWR from 'swr';
 import nookies from 'nookies';
 
 import { verifyIdToken } from '../../utils/firebaseAdmin';
-import {
-  AnimatedPing,
-  Layout,
-  FlexCol,
-  FlexCenter,
-  Navbar,
-  Container,
-  Link,
-} from '../../components';
-import { startJob, stopJob } from '../../utils/handleJob';
+import { Layout, FlexCenter, JobButton, Navbar, Container, Link } from '../../components';
 import { dateFormatter } from '../../utils/date';
-
-const JobButton = ({ jobId, jobStatus }) => {
-  const isInitialized = jobStatus === 'initialized';
-  const isRunning = jobStatus === 'running';
-  const isPending = jobStatus === 'starting';
-  return (
-    <div
-      className={`py-1 text-center rounded-lg lg:transition lg:duration-100 text-white w-32
-	      ${isInitialized ? 'bg-green-800 hover:bg-green-900 cursor-pointer' : ''}
-	      ${isRunning ? 'bg-red-800 hover:bg-red-900 cursor-pointer' : ''}
-	      ${isPending ? 'bg-yellow-800' : ''}
-	      `}
-      onClick={() => {
-        if (isInitialized) startJob(jobId);
-        if (isRunning) stopJob(jobId);
-      }}
-    >
-      {isInitialized && 'START'}
-      {isRunning && 'STOP'}
-      {isPending && (
-        <div>
-          <div className="inline-block mr-3">PENDING</div>
-          <AnimatedPing />
-        </div>
-      )}
-    </div>
-  );
-};
 
 const JobsTable = ({ data }) => {
   const headersName = ['Id', 'Name', 'Created at', 'Finished at', 'Started at', 'Status', '', ''];
