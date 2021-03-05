8785dd88bb855fef4a306c869f8ebd6b02fe7186
Simeon Carstens <simeon.carstens@tweag.io>
2021-03-05 11:19:55 +0100

    Add black v20.8b1 to Nix shell

--- shell.nix
+++ shell.nix
@@ -1,12 +1,44 @@
 { pkgs ? import ./nix {} }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    python38
-    python38Packages.poetry
-    niv.niv
-    yarn
-    docker-compose
-    openmpi
-    python38Packages.tkinter
-  ];
-}
+with pkgs.python38Packages;
+let
+  black = buildPythonPackage rec {
+    pname = "black";
+    version = "20.8b1";
+    src = fetchPypi {
+      inherit version pname;
+      sha256 = "1c02557aa099101b9d21496f8a914e9ed2222ef70336404eeeac8edba836fbea";
+    };
+    buildInputs = [
+      setuptools_scm
+      regex
+      click
+      pathspec
+      typed-ast
+      toml
+      mypy-extensions
+      appdirs
+      typing-extensions
+    ];
+    propagatedBuildInputs = [
+      regex
+      typing-extensions
+      mypy-extensions
+      toml
+      typed-ast
+      pathspec
+      ];
+    doCheck = false;
+  };
+in 
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      python38
+      python38Packages.poetry
+      black
+      niv.niv
+      yarn
+      docker-compose
+      openmpi
+      python38Packages.tkinter
+    ];
+  }
