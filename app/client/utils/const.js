acf9207e492ad6004f222dc3da3f7a1962afe81d
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-03-08 18:23:17 +0100

    intermediate

--- app/client/utils/const.js
+++ app/client/utils/const.js
@@ -11,5 +11,5 @@ export const GRAPHITE_URL = process.env.GRAPHITE_URL || 'http://127.0.0.1';
 export const GRAPHITE_PORT = process.env.GRAPHITE_PORT || '80';
 export const GRAPHITE_NEGLOGP_URL = (jobId, simulationRun) =>
   `${GRAPHITE_URL}:${GRAPHITE_PORT}/render?target=aggregate(${jobId}.${simulationRun}.*.negative_log_prob,'sum')&format=json&from=-5min`;
-export const GRAPHITE_ACCEPTANCE_RATE_URL = (jobId) =>
+export const GRAPHITE_ACCEPTANCE_RATE_URL = (jobId, simulationRun) =>
   `${GRAPHITE_URL}:${GRAPHITE_PORT}/render?target=${jobId}.${simulationRun}.replica*_replica*.acceptance_rate&format=json`;
