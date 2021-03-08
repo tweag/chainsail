6d6209ae8b2b28189dffd934f41317d8d59fad14
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-03-08 21:18:50 +0100

    Use scheduler urls in api

--- app/client/pages/api/job/get/[jobId].js
+++ app/client/pages/api/job/get/[jobId].js
@@ -1,9 +1,9 @@
-import { FLASK_URL, JOB_GET_ENDPOINT } from '../../../../utils/const';
+import { JOB_GET_URL } from '../../../../utils/const';
 import handleRequestResponse from '../../../../utils/handleRequestResponse';
 
 export default async (req, res) => {
   const { jobId } = req.query;
-  const url = `${FLASK_URL}${JOB_GET_ENDPOINT(jobId)}`;
+  const url = JOB_GET_URL(jobId);
   const method = 'GET';
   handleRequestResponse(req, res, url, method);
 };
