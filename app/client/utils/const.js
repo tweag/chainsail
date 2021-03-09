775ae6540c4b99930a06164bcfb5d69210602640
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-03-09 13:11:34 +0100

    Graphite port number is 8080

--- app/client/utils/const.js
+++ app/client/utils/const.js
@@ -7,7 +7,7 @@ export const JOB_GET_URL = (jobId) => `${SCHEDULER_URL}/job/${jobId}`;
 export const JOBS_LIST_URL = `${SCHEDULER_URL}/jobs`;
 
 // Graphite
-export const GRAPHITE_URL = process.env.GRAPHITE_URL || 'http://127.0.0.1:80';
+export const GRAPHITE_URL = process.env.GRAPHITE_URL || 'http://127.0.0.1:8080';
 export const GRAPHITE_NEGLOGP_URL = (jobId, simulationRun) =>
   `${GRAPHITE_URL}/render?target=aggregate(${jobId}.${simulationRun}.*.negative_log_prob,'sum')&format=json&from=-5min`;
 export const GRAPHITE_ACCEPTANCE_RATE_URL = (jobId, simulationRun) =>
