6940c21136916466920256347e588bbdb76ebbcb
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-03-12 17:31:44 +0100

    intermdeiate

--- app/scheduler/resaas/scheduler/app.py
+++ app/scheduler/resaas/scheduler/app.py
@@ -121,7 +121,7 @@ def stop_job(job_id, user_id):
     return ("ok", 200)
 
 
-@app.route("/job/<job_id>/update_signed_url", methods=["POST"])
+@app.route("/job/<job_id>/update_signed_url", methods=["GET"])
 @check_user
 def update_job_signed_url(job_id, user_id):
     find_job(job_id, user_id)
