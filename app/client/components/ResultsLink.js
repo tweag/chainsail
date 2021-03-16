9db41e137ede25e87007e6c8b4c98723664d2057
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-03-16 08:10:39 +0100

    Export download results only if the signed_url is not empty

--- app/client/components/ResultsLink.js
+++ app/client/components/ResultsLink.js
@@ -1,10 +1,16 @@
-const ResultsLink = ({ signed_url }) => (
-  <a
-    download
-    href={signed_url}
-    className="w-32 py-1 text-center text-white bg-green-600 rounded-lg cursor-pointer lg:transition lg:duration-100 hover:bg-green-700"
-  >
-    Download results
-  </a>
-);
+const ResultsLink = ({ signed_url }) => {
+  if (signed_url) {
+    return (
+      <a
+        download
+        href={signed_url}
+        className="w-32 py-1 text-center text-white bg-green-600 rounded-lg cursor-pointer lg:transition lg:duration-100 hover:bg-green-700"
+      >
+        Download results
+      </a>
+    );
+  } else {
+    return <></>;
+  }
+};
 export default ResultsLink;
