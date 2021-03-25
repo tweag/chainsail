9159b905cc0d463b310f1537095e5bd7ba9fb657
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-03-25 08:56:54 +0100

    Replace line breake with flex column structure

--- app/client/pages/index.js
+++ app/client/pages/index.js
@@ -7,44 +7,47 @@ const Heading = () => (
       sampling multimodal distributions made easy
     </div>
     <div className="text-base md:text-xl lg:w-2/3 md:text-justify mb-7">
-      RESAAS helps to sample multimodal distributions by implementing an automatically scaling
-      Replica Exchange MCMC algorithm. All you need to provide is a Python module defining your
-      probability density and its log-probability gradient.
-      <br />
-      This beta version of RESAAS is free (you have a compute time quota of 20 replicas x 2 hours).
-      <br />
-      <br />
-      To learn more about the MCMC algorithms used in RESAAS, see our blog posts:
-      <br />-{' '}
-      <span className="underline">
-        <a
-          style={{ display: 'table-cell' }}
-          href="https://www.tweag.io/blog/2020-10-28-mcmc-intro-4/"
-          target="_blank"
-        >
-          Replica Exchange
-        </a>
-      </span>
-      <br />-{' '}
-      <span className="underline">
-        <a
-          style={{ display: 'table-cell' }}
-          href="https://www.tweag.io/blog/2020-08-06-mcmc-intro3/"
-          target="_blank"
-        >
-          Hamiltonian Monte Carlo
-        </a>
-      </span>
-      <br />
+      <FlexCol className="space-y-5">
+        <div>
+          RESAAS helps to sample multimodal distributions by implementing an automatically scaling
+          Replica Exchange MCMC algorithm. All you need to provide is a Python module defining your
+          probability density and its log-probability gradient.
+        </div>
+        <div>
+          This beta version of RESAAS is free (you have a compute time quota of 20 replicas x 2
+          hours).
+        </div>
+        <FlexCol className="space-y-2">
+          <div>To learn more about the MCMC algorithms used in RESAAS, see our blog posts:</div>
+          <FlexCol>
+            <a
+              href="https://www.tweag.io/blog/2020-10-28-mcmc-intro-4/"
+              target="_blank"
+              className="transition duration-300 hover:opacity-50"
+            >
+              <i class="fas fa-chevron-right ml-2 mr-5"></i>
+              Replica Exchange
+            </a>
+            <a
+              href="https://www.tweag.io/blog/2020-08-06-mcmc-intro3/"
+              target="_blank"
+              className="transition duration-300 hover:opacity-50"
+            >
+              <i class="fas fa-chevron-right ml-2 mr-5"></i>
+              Hamiltonian Monte Carlo
+            </a>
+          </FlexCol>
+        </FlexCol>
+      </FlexCol>
     </div>
-    <Button href="/job" className="bg-purple-600 hover:bg-purple-500">
+    <Button href="/job" className="bg-purple-600 hover:bg-purple-700">
       Give it a whirl
     </Button>
   </FlexCol>
 );
 
 const CopyrightFooter = () => (
-  <FlexCenter className="h-10 mx-10 md:mx-20 lg:mx-40 text-xs">
+  <FlexCenter className="h-10 mx-10 text-xs md:mx-20 lg:mx-40">
     <div className="opacity-30">All rights reserved. Copyright © 2021 by</div>
     <a href="https://www.tweag.io/">
       <img
