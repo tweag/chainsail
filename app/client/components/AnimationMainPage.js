9496e625495c189fdbdb391fb9c8d0f721c3ded7
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-03-30 08:22:24 +0200

    Start animation from negative x values

--- app/client/components/AnimationMainPage.js
+++ app/client/components/AnimationMainPage.js
@@ -8,20 +8,26 @@ function ReactSpring({ duration }) {
 
   const xmin = -5;
   const xmax = 5;
-  const n_hills = Math.floor(Math.random() * 2) + 1;
+  const n_hills = Math.floor(Math.random() * 3) + 1;
   const n_hills_range = [...Array(n_hills).keys()];
   const means = n_hills_range.map(() => Math.random() * (xmax / 2 - xmin / 2) + xmin / 2);
   const sigmas = n_hills_range.map(() => Math.random() * 0.2 + 0.5);
   const pathFunction = (x) => {
-    let y_unnorm = n_hills_range
-      .map((i) => (1 / sigmas[i]) * Math.exp(-Math.pow((x - means[i]) / sigmas[i], 2) / 2))
-      .reduce((a, b) => a + b, 0);
-    return y_unnorm / n_hills;
+    if (x >= xmin) {
+      let y_unnorm = n_hills_range
+        .map((i) => (1 / sigmas[i]) * Math.exp(-Math.pow((x - means[i]) / sigmas[i], 2) / 2))
+        .reduce((a, b) => a + b, 0);
+      return y_unnorm / n_hills;
+    } else {
+      return 0;
+    }
   };
 
   const N = 3000;
+  const eps = 0.3;
   const offset = (Math.random() + 0.2) / 1.2;
-  let globalPath = [...Array(N).keys()]
+  let globalPath = [...Array(Math.floor(N * (1 + eps))).keys()]
+    .map((i) => i - Math.floor(N * eps))
     .map((i) => xmin + (xmax - xmin) * (i / N))
     .map((x) => {
       let y = pathFunction(x);
@@ -29,7 +35,6 @@ function ReactSpring({ duration }) {
       let y_norm = -(y / (xmax - xmin)) * outerWidth + offset * outerHeight;
       return `${x_norm} ${y_norm}`;
     });
-  const eps = 0.3;
   let interpolate = (t) => {
     let path = globalPath.slice(Math.floor(t * N), Math.floor((t + eps) * N)).join(' L');
     path = 'M' + path;
