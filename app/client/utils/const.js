25a1203e34e1796b2ccd93fa75864c7ff26c2ff2
Simeon Carstens <simeon.carstens@tweag.io>
2021-03-31 17:13:48 +0000

    Add default value for simulationRun argument to Graphite stats routes
    Also replace explicit path by Xses in docker-compose.yaml

--- app/client/utils/const.js
+++ app/client/utils/const.js
@@ -8,9 +8,9 @@ export const JOBS_LIST_URL = `${SCHEDULER_URL}/jobs`;
 
 // Graphite
 export const GRAPHITE_URL = process.env.GRAPHITE_URL || 'http://127.0.0.1:8080';
-export const GRAPHITE_NEGLOGP_URL = (jobId, simulationRun) =>
+export const GRAPHITE_NEGLOGP_URL = (jobId, simulationRun = '*') =>
   `${GRAPHITE_URL}/render?target=aggregate(job${jobId}.${simulationRun}.*.negative_log_prob,'sum')&format=json&from=-3hours&until=now`;
-export const GRAPHITE_ACCEPTANCE_RATE_URL = (jobId, simulationRun) =>
+export const GRAPHITE_ACCEPTANCE_RATE_URL = (jobId, simulationRun = '*') =>
   `${GRAPHITE_URL}/render?target=job${jobId}.${simulationRun}.replica*_replica*.acceptance_rate&format=json&from=-3hours&until=now`;
 export const GRAPHITE_LOGS_URL = (jobId) =>
   `${GRAPHITE_URL}/events/get_data?tags=log&tags=job${jobId}&from=-3hours&until=now`;
