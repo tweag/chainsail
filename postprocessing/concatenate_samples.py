1d6435480461f49984e36780d3ae1a31c279c7d4
Simeon Carstens <simeon.carstens@tweag.io>
2021-04-16 16:49:22 +0200

    Run formatter

--- postprocessing/concatenate_samples.py
+++ postprocessing/concatenate_samples.py
@@ -6,9 +6,16 @@ import os
 
 import numpy as np
 
-parser = argparse.ArgumentParser(description="Concatenate batches of MCMC samples into a single .npy file")
+parser = argparse.ArgumentParser(
+    description="Concatenate batches of MCMC samples into a single .npy file"
+)
 parser.add_argument("out", type=str, help="Output file")
-parser.add_argument("--simulation_run", type=str, default="production_run", help='Simulation run, e.g. "optimization_run1" or "production_run". Default: "production_run"')
+parser.add_argument(
+    "--simulation_run",
+    type=str,
+    default="production_run",
+    help='Simulation run, e.g. "optimization_run1" or "production_run". Default: "production_run"',
+)
 args = parser.parse_args()
 
 with open(os.path.join(args.simulation_run, "config.yml")) as f:
