d2be25b0d3690ec762b7ee10df8a5f033ca4e6dc
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-06-11 11:35:55 +0200

    Fix: button width should be fixed instead of propotional

--- app/client/components/Button.js
+++ app/client/components/Button.js
@@ -4,7 +4,7 @@ export default function Layout({ children, href, className }) {
   return (
     <Link href={href}>
       <div
-        className={`px-6 pt-3 pb-4 text-base text-center rounded-lg cursor-pointer md:text-xl lg:w-1/6 lg:transition lg:duration-300 ${className}`}
+        className={`px-6 pt-3 pb-4 text-base text-center rounded-lg cursor-pointer md:text-xl lg:w-52 lg:transition lg:duration-300 ${className}`}
       >
         {children}
       </div>
