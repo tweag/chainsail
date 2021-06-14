4ce7d6481607f33eb0e7500bddd14e68d8fc1dc1
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-06-14 14:15:39 +0200

    Fix space between from and description components

--- app/client/pages/job.js
+++ app/client/pages/job.js
@@ -283,7 +283,12 @@ const Job = ({ authed }) => {
                     .
                   </div>
                 </FlexCol>
-                <FlexRow between responsive media="lg" className="w-full lg:h-4/5 lg:space-x-20">
+                <FlexRow
+                  between
+                  responsive
+                  media="lg"
+                  className="w-full space-y-28 lg:space-y-0 lg:h-4/5 lg:space-x-10"
+                >
                   <FlexCenter className="flex-grow mb-10 lg:py-10 h-96 md:h-80 lg:h-full lg:mb-0 lg:w-1/2">
                     <form
                       className="h-full"
