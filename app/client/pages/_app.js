5ae6e9244a433173640b2b550cde36ee577d75be
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-06-30 12:42:06 +0200

    Add isMobile scope to all pages

--- app/client/pages/_app.js
+++ app/client/pages/_app.js
@@ -3,11 +3,17 @@ import 'katex/dist/katex.min.css';
 import '../style/global.css';
 
 import { AuthProvider } from '../components/Auth';
+import useWindowDimensions from '../utils/windowDimensions';
+import { sm } from '../utils/breakPoints';
 
 function MyApp({ Component, pageProps }) {
+  // To check if the screen is for mobile
+  const { width } = useWindowDimensions();
+  const isMobile = width <= sm;
+
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <Component {...pageProps} isMobile={isMobile} />
     </AuthProvider>
   );
 }
