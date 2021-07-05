7fba81d2ca0e43e4546b14c124b6fe99b567ae41
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-07-05 10:10:13 +0200

    Use normal size table for non mobile devices

--- app/client/pages/results.js
+++ app/client/pages/results.js
@@ -3,10 +3,91 @@ import nookies from 'nookies';
 import { v4 as uuidv4 } from 'uuid';
 
 import { verifyIdToken } from '../utils/firebaseAdmin';
-import { Layout, FlexCenter, FlexCol, Navbar, Container, Link } from '../components';
+import {
+  Layout,
+  FlexCenter,
+  FlexCol,
+  JobButton,
+  Navbar,
+  Container,
+  Link,
+  ResultsLink,
+} from '../components';
 import JobInfo from '../components/JobInfo';
 import fetcher from '../utils/fetcher';
 import { useState } from 'react';
+import { dateFormatter } from '../utils/date';
+
+const JobsTableForNonMobile = ({ data }) => {
+  const headersName = [
+    'Id',
+    'Name',
+    'Created at',
+    'Started at',
+    'Finished at',
+    'Results',
+    'Status',
+    '',
+    '',
+  ];
+  const TableHeader = ({ children }) => (
+    <th className="px-2 py-1 text-left lg:px-4 lg:py-2">{children}</th>
+  );
+  const TableRow = ({ row }) => {
+    const job_name = JSON.parse(row.spec).name;
+    return (
+      <tr className="hover:bg-gray-800 transition duration-100">
+        <TableData d={row.id} />
+        <TableData d={job_name} />
+        <TableData d={dateFormatter(row.created_at)} />
+        <TableData d={dateFormatter(row.started_at)} />
+        <TableData d={dateFormatter(row.finished_at)} />
+        <TableData className="w-48">
+          <ResultsLink signed_url={row.signed_url} width="w-20 lg:w-32" />
+        </TableData>
+        <TableData d={row.status} className="w-40" />
+        <TableData className="w-48">
+          <Link href={`/dash?jobId=${row.id}`}>
+            <div className="w-16 py-1 text-center text-white bg-purple-600 rounded-lg cursor-pointer lg:w-32 lg:transition lg:duration-100 hover:bg-purple-700">
+              dash site
+            </div>
+          </Link>
+        </TableData>
+        <TableData className="w-48">
+          <JobButton jobId={row.id} jobStatus={row.status} width="w-24 lg:w-32" />
+        </TableData>
+      </tr>
+    );
+  };
+  const TableData = ({ d, children, className }) => (
+    <td
+      className={`px-2 py-1 text-left lg:px-4 lg:py-2 border-t-2
+		  ${className}`}
+    >
+      {d ? d : children}
+    </td>
+  );
+  return (
+    <div className="w-full overflow-x-auto text-xs text-white bg-gray-900 rounded-lg shadow-xl lg:text-base">
+      <table className="w-full">
+        <thead>
+          <tr className="bg-blue-900 hover:bg-blue-800">
+            {headersName.map((h) => (
+              <TableHeader key={uuidv4()}>{h}</TableHeader>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data
+            .sort((a, b) => (a.id > b.id ? 1 : -1))
+            .map((row) => (
+              <TableRow row={row} key={uuidv4()} />
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 const JobsTableForMobile = ({ data }) => {
   // To keep track of clicked job row
@@ -103,6 +184,9 @@ const Results = ({ authed, isMobile }) => {
               {!error && Array.isArray(data) && data.length == 0 && <div>no jobs created yet</div>}
               {!error && Array.isArray(data) && data.length > 0 && isMobile && (
                 <JobsTableForMobile data={data} />
+              )}
+              {!error && Array.isArray(data) && data.length > 0 && !isMobile && (
+                <JobsTableForNonMobile data={data} />
               )}
             </FlexCenter>
           </Container>
