3731887030e1bc8ade53dda52864724c870b6573
Etienne JEAN <et.jean@outlook.fr>
2021-08-25 14:55:34 +0200

    Start to implement create, restart, delete methods for K8SNode implementation

--- app/scheduler/chainsail/scheduler/nodes/k8s_pod.py
+++ app/scheduler/chainsail/scheduler/nodes/k8s_pod.py
@@ -12,6 +12,11 @@ from chainsail.scheduler.config import (
 )
 from chainsail.scheduler.db import TblJobs, TblNodes
 from chainsail.scheduler.nodes.base import Node, NodeType, NodeStatus
+import kubernetes as kub
+
+
+
+
 
 
 
@@ -31,34 +36,87 @@ class K8sNode(Node):
         self,
         name: str,
         config: GeneralNodeConfig,
+        spec: JobSpec,
         representation: Optional[TblNodes] = None,
         status: Optional[NodeStatus] = None,
     ):
         self._name = name
         self._representation = representation
         self._config = config
+        self.spec = spec
         if not status:
             self._status = NodeStatus.INITIALIZED
         else:
             self._status = status
         # TODO address field
         self._address = None
-        
-
+        self._pod = None
     
     def create(self) -> Tuple[bool, str]:
         # TODO
-        print("CREATE POD")
+        # Load kubernetes config
+        kub.config.load_kub_config()
+        # Define pod's containers
+        httpstan_container = kub.client.V1Container(
+            name="httpstan",
+            image=self._config.httpstan_image,
+            env=[kub.client.V1EnvVar(name='HTTPSTAN_PORT', value='8082')],
+            host_network=True,
+            #TODO: Add something to replace `--log-driver=gcplogs` -> See this issue : https://github.com/kubernetes/kubernetes/issues/15478
+        )
+        user_code_container = kub.client.V1Container(
+            name="user_code",
+            image=self._config.user_code_image,
+            args=["python", "/app/app/user_code_server/chainsail/user_code_server/__init__.py"],
+            ports=[kub.client.V1ContainerPort(container_port=50052)],
+            env=[
+                kub.client.V1EnvVar(name='USER_PROB_URL', value=self.spec.probability_definition),
+                kub.client.V1EnvVar(name='USER_INSTALL_SCRIPT', value=None), #TODO: fill the value for install script
+                kub.client.V1EnvVar(name='USER_CODE_SERVER_PORT', value=50052),
+                kub.client.V1EnvVar(name='REMOTE_LOGGING_CONFIG_PATH', value='/chainsail/remote_logging.yaml'),
+            ],
+            volume_mounts=[
+                #TODO: volume for remote_logging.yaml
+                #TODO: volume for install_script
+            ],
+            host_network=True,
+            #TODO: Add something to replace `--log-driver=gcplogs` -> See this issue : https://github.com/kubernetes/kubernetes/issues/15478
+        )
+        container = kub.client.V1Container(
+            name="rex",
+            image=self._config.image,
+            args=[self._config.cmd] + self._config.args,
+            ports=[kub.client.V1ContainerPort(container_port=50051)],
+            volume_mounts=[
+                #TODO: volume for config_dir
+                #TODO: volume for authorized_keys
+                #TODO: volume for pem_file
+            ],
+            host_network=True,
+            #TODO: Add something to replace `--log-driver=gcplogs` -> See this issue : https://github.com/kubernetes/kubernetes/issues/15478
+        )
+        # Define pod
+        self._pod = kub.client.V1Pod(
+            api_version="v1",
+            kind="Pod",
+            metadata=kub.client.V1ObjectMeta(name=self._name, labels={"app":"rex-pod"}),
+            spec=kub.client.V1PodSpec(containers=[httpstan_container, user_code_container, container])
+        )
+        # Create pod
+        core_v1 = kub.client.CoreV1Api()
+        response = core_v1.create_namespaced_pod(body=self._pod, namespace="default")
         return (True, "LOGS FROM CREATE...")
     
     def restart(self) -> bool:
         # TODO
-        print("RESTART POD")
+        core_v1 = kub.client.CoreV1Api()
+        response = core_v1.patch_namespaced_pod(name=self._name, body=self._pod, namespace="default")
         return True
     
     def delete(self) -> bool:
         # TODO
-        print("DELETEPOD")
+        core_v1 = kub.client.CoreV1pi()
+        response = core_v1.delete_namespaced_pod(name=self._name, namespace="default")
         return True
     
     @property
