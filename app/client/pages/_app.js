e2118271958289ca1575ce69834811b0fbab193b
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2021-10-01 15:36:08 +0200

    WIP: works only if page starts on authed page

--- app/client/pages/_app.js
+++ app/client/pages/_app.js
@@ -7,24 +7,30 @@ import useWindowDimensions from '../utils/windowDimensions';
 import { sm } from '../utils/breakPoints';
 import { useEffect } from 'react';
 
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
+
 function MyApp({ Component, pageProps }) {
   useEffect(()=> {
     try {
-      // you can import these packages anywhere
-      const LogRocket = require('logrocket');
-      const setupLogRocketReact = require('logrocket-react');
-      
       // only initialize when in the browser
       if (window !== undefined) {
         LogRocket.init('9snsmi/chainsail');
-        setupLogRocketReact(LogRocket);
         
-        const {uid, email} = pageProps;
-        if(uid !== undefined && email !== undefined) {
-          LogRocket.identify(uid, {
-            email: email,
-          });
+        try {
+          console.log("pageProps=" + JSON.stringify(pageProps));
+          const {uid, email} = pageProps;
+          if(uid !== undefined && email !== undefined) {
+            LogRocket.identify(uid, {
+              email: email,
+            });
+            console.log("Identified to LogRocket")
+          }
+        } catch (e) {
+          console.log("Identify failed on " + JSON.stringify({uid, email}) + ", session is anonym");
         }
+
+        setupLogRocketReact(LogRocket);
       }
     } catch(e) {
       console.log(e);
