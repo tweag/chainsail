d9f81a8fea88a665da2223061cea450e42ffddc4
Simeon Carstens <simeon.carstens@tweag.io>
2021-11-01 19:11:42 +0100

    Add MCMC stats server to docker-compose and nginx

--- app/mcmc_stats_server/chainsail/mcmc_stats_server/app.py
+++ app/mcmc_stats_server/chainsail/mcmc_stats_server/app.py
@@ -13,12 +13,16 @@ from chainsail.common.storage import load_storage_backend, SimulationStorage
 app = Flask(__name__)
 
 storage_config = yaml.safe_load(open(os.getenv("STORAGE_CONFIG")))
+
 storage_backend = load_storage_backend("cloud", storage_config["backend_config"]["cloud"])
+basename = os.getenv("STORAGE_BASENAME")
+if not basename:
+    raise ValueError("STORAGE_BASENAME not set")
 
 
-@app.route("/logs/<job_id>/<simulation_run>/neg_log_prob_sum", methods=["GET"])
+@app.route("/mcmc_stats/<job_id>/<simulation_run>/neg_log_prob_sum", methods=["GET"])
 def neg_log_prob_sum(job_id, simulation_run):
-    storage = SimulationStorage("storage", f"{job_id}/{simulation_run}", storage_backend)
+    storage = SimulationStorage(basename, f"{job_id}/{simulation_run}", storage_backend)
     energies = storage.load_all_energies()
     dump_step = storage.load_config()["re"]["dump_step"]
     summed_energies = energies.sum(0)
@@ -26,11 +30,11 @@ def neg_log_prob_sum(job_id, simulation_run):
                     for i, summed_energy in enumerate(summed_energies)})
 
 
-@app.route("/logs/<job_id>/<simulation_run>/re_acceptance_rates", methods=["GET"])
+@app.route("/mcmc_stats/<job_id>/<simulation_run>/re_acceptance_rates", methods=["GET"])
 def re_acceptance_rates(job_id, simulation_run):
     client = storage.Client()
     bucket = client.bucket(storage_config["backend_config"]["cloud"]["container_name"])
-    config_blob = bucket.blob(f"storage/{job_id}/{simulation_run}/statistics/re_stats.txt")
+    config_blob = bucket.blob(f"{basename}/{job_id}/{simulation_run}/statistics/re_stats.txt")
     stats = np.loadtxt(StringIO(config_blob.download_as_text()), dtype=float)
     return jsonify({int(step_data[0]): list(step_data[1:]) for step_data in stats})
  
