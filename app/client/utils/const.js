5797ee13c1d661b7b83f43c50d727c26a63347da
Simeon Carstens <simeon.carstens@tweag.io>
2021-11-03 00:37:38 +0100

    WIP

--- app/client/utils/const.js
+++ app/client/utils/const.js
@@ -8,9 +8,12 @@ export const JOBS_LIST_URL = `${SCHEDULER_URL}/jobs`;
 
 // Graphite
 export const GRAPHITE_URL = process.env.GRAPHITE_URL || 'http://127.0.0.1:8080';
-export const GRAPHITE_NEGLOGP_URL = (jobId, simulationRun) =>
-  `${GRAPHITE_URL}/render?target=aggregate(job${jobId}.${simulationRun}.*.negative_log_prob,'sum')&format=json&from=-3hours&until=now`;
-export const GRAPHITE_ACCEPTANCE_RATE_URL = (jobId, simulationRun) =>
-  `${GRAPHITE_URL}/render?target=job${jobId}.${simulationRun}.replica*_replica*.acceptance_rate&format=json&from=-3hours&until=now`;
 export const GRAPHITE_LOGS_URL = (jobId) =>
   `${GRAPHITE_URL}/events/get_data?tags=log&tags=job${jobId}&from=-3hours&until=now`;
+
+// MCMC stats
+export const MCMC_STATS_URL = process.env.MCMC_STATS_URL || 'http://127.0.0.1:8081';
+export const MCMC_STATS_NEGLOGP_URL = (jobId, simulationRun) =>
+  `${MCMC_STATS_URL}/mcmc_stats/${jobID}/${simulationRun}/neg_log_prob_sum`;
+export const MCMC_STATS_ACCEPTANCE_RATE_URL = (jobId, simulationRun) =>
+  `${MCMC_STATS_URL}/mcmc_stats/${jobId}/${simulationRun}/re_acceptance_rates`;
