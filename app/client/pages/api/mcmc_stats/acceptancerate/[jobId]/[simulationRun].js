40b4ab599070d849fbf32c39ce8ba44790c3be6e
Simeon Carstens <simeon.carstens@tweag.io>
2021-11-12 01:22:25 -0800

    Adapt dash plots to MCMC stats server (#347)
    * WIP
    
    * WIP
    
    * Done
    
    * Formatting, apparently
    
    * Remove legacy route
    
    * Make dash plots not ask for data when simulationRun is undefined

--- app/client/pages/api/mcmc_stats/acceptancerate/[jobId]/[simulationRun].js
+++ app/client/pages/api/mcmc_stats/acceptancerate/[jobId]/[simulationRun].js
@@ -1,9 +1,9 @@
-import { GRAPHITE_NEGLOGP_URL } from '../../../../../utils/const';
+import { MCMC_STATS_ACCEPTANCE_RATE_URL } from '../../../../../utils/const';
 import handleRequestResponse from '../../../../../utils/handleRequestResponse';
 
 export default async (req, res) => {
   const { jobId, simulationRun } = req.query;
-  const url = GRAPHITE_NEGLOGP_URL(jobId, simulationRun);
+  const url = MCMC_STATS_ACCEPTANCE_RATE_URL(jobId, simulationRun);
   const method = 'GET';
   const checkAuth = false;
   await handleRequestResponse(req, res, url, method, checkAuth);
