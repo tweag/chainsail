4f45a96f4241e476a35ce7b1a66f1e62de29dacb
dorranh <dorran.howell@tweag.io>
2022-03-11 14:21:53 +0100

    Fix error in entrypoint from manual conflict resolution

--- docker/node/entrypoint.sh
+++ docker/node/entrypoint.sh
@@ -4,12 +4,4 @@ set -ex
 
 mkdir -p /root/.ssh
 
-# wait up to 5 minutes for user code server to be ready
-# during that time, the user code server can install dependencies
-if wait-for-it -t 300 localhost:50052; then
-    echo User code gRPC server is ready
-    exec "$@"
-else
-    echo User code gRPC server unreachable
-    exit 1
-fi
+exec "$@"
