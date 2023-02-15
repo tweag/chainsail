5e27b596bc736a2642addd8a763a3eabb6e37ae0
Simeon Carstens <simeon.carstens@tweag.io>
2023-02-15 17:51:38 +0100

    Don't require Firebase authentication by default
    This makes sense, I think, and also prevents us from having to have a
    dedicated `next.config.js.example`, because `next.config.js` as it is
    in this commit works just fine.

--- app/client/next.config.js
+++ app/client/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     serverRuntimeConfig: {
         // Switch to enable / disable mandatory Firebase authentication
-        require_auth: true,
+        require_auth: false,
         // Google Secret Manager secret that holds the Firebase admin credentials.
         // Can have an arbitrary value as long as the above option is set to `false`.
         secret_name: 'TODO',
