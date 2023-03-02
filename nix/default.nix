43efe89c5bd2105cc57e4e9502b5fd7c1a0f7f5d
Steven Shaw <steven.shaw@tweag.io>
2023-03-02 16:14:36 +1000

    Switch to a flake-first approach for the Nix shell
    This will allow an easier path for pinning poetry and poetry2nix.

--- nix/default.nix
+++
@@ -1,9 +0,0 @@
-let
-  lock = builtins.fromJSON (builtins.readFile ../flake.lock);
-  inherit (lock.nodes.nixpkgs.locked) rev narHash;
-  nixpkgs = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = narHash;
-  };
-in
-import nixpkgs
