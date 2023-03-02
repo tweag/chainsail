b1366ddad3a69ea22ead8e97cae2bbfea7ee3675
Steven Shaw <steven.shaw@tweag.io>
2023-03-02 16:14:36 +1000

    Switch to a flake-first approach for the Nix shell
    This will allow an easier path for pinning poetry and poetry2nix.

--- flake.nix
+++ flake.nix
@@ -8,9 +8,30 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      {
-        devShell = import ./shell.nix { inherit pkgs; };
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            docker-compose
+            file
+            gnumake
+            kubectl
+            kubernetes-helm
+            minikube
+            ncurses
+            nodejs
+            openmpi
+            poetry # version 1.1.10 (required) while pythonPackages38.poetry is lower
+            python38Packages.tkinter
+            terraform
+            yarn
+            zlib
+          ];
+
+          # Setting the LD_LIBRARY_PATH environment variable.
+          # Can also make use of the `.overrideAttrs` method to prevent from overwriting it.
+          # See PR #310 for details: https://github.com/tweag/chainsail/pull/310.
+          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.file}/lib:${pkgs.zlib}/lib";
+        };
       }
     );
 }
