dd693777976bee1f9d943c42e2987f246bc6e16f
Steven Shaw <steven.shaw@tweag.io>
2023-03-02 14:56:18 +1000

    Sort packages in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,19 +1,19 @@
 { pkgs ? import ./nix { } }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    poetry # version 1.1.10 (required) while pythonPackages38.poetry is lower
-    yarn
-    nodejs
     docker-compose
-    openmpi
-    python38Packages.tkinter
-    ncurses
     file
     gnumake
-    terraform
+    kubectl
     kubernetes-helm
     minikube
-    kubectl
+    ncurses
+    nodejs
+    openmpi
+    poetry # version 1.1.10 (required) while pythonPackages38.poetry is lower
+    python38Packages.tkinter
+    terraform
+    yarn
   ];
   # Setting the LD_LIBRARY_PATH environment variable.
   # Can also make use of the `.overrideAttrs` medthod to prevent from overwriting it (See PR #310 for details)
