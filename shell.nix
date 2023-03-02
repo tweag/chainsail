8d3d2f05ab3e104beb2278947073ffc0ebf2c4e2
Steven Shaw <steven.shaw@tweag.io>
2023-03-02 15:00:45 +1000

    Add zlib to get chainsail-controller-local to work

--- shell.nix
+++ shell.nix
@@ -14,8 +14,11 @@ pkgs.mkShell {
     python38Packages.tkinter
     terraform
     yarn
+    zlib
   ];
+
   # Setting the LD_LIBRARY_PATH environment variable.
-  # Can also make use of the `.overrideAttrs` medthod to prevent from overwriting it (See PR #310 for details)
-  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.file}/lib";
+  # Can also make use of the `.overrideAttrs` method to prevent from overwriting it.
+  # See PR #310 for details: https://github.com/tweag/chainsail/pull/310.
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.file}/lib:${pkgs.zlib}/lib";
 }
