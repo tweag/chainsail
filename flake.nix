d1f61280ac1869a6c265011f350daafe9d8b24fd
Steven Shaw <steven.shaw@tweag.io>
2023-03-03 14:01:35 +1000

    Upgrade Poetry and create Nix package for controller

--- flake.nix
+++ flake.nix
@@ -4,13 +4,19 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/release-22.11";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, poetry2nix, flake-utils }:
+  outputs = { self, flake-utils, nixpkgs, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
-       let
-         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        poetry2nixPkg = poetry2nix.legacyPackages.${system};
+      in
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             docker-compose
@@ -33,6 +39,28 @@
           # Can also make use of the `.overrideAttrs` method to prevent from overwriting it.
           # See PR #310 for details: https://github.com/tweag/chainsail/pull/310.
           LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.file}/lib:${pkgs.zlib}/lib";
+        };
+        packages = {
+          controller = poetry2nixPkg.mkPoetryApplication {
+            projectDir = ./app/controller;
+            overrides = poetry2nixPkg.overrides.withDefaults (
+              final: prev:
+                let
+                  addNativeBuildInputs = drvName: inputs: {
+                    "${drvName}" = prev.${drvName}.overridePythonAttrs (
+                      old: {
+                        nativeBuildInputs =
+                          (old.nativeBuildInputs or [ ]) ++ inputs;
+                      }
+                    );
+                  };
+                in
+                { }
+                // addNativeBuildInputs "mpi4py" [ final.cython ]
+                // addNativeBuildInputs "rexfw" [ final.poetry-core ]
+            );
+          };
+          default = self.packages.${system}.controller;
         };
       }
     );
