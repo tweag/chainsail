360eb1fd9f7ffc148817261bfc4d3e0e3d94b70e
Steven Shaw <steven.shaw@tweag.io>
2023-03-07 13:44:42 +1000

    Create devShell for controller

--- flake.nix
+++ flake.nix
@@ -24,51 +24,61 @@
         ourYarn = nodePkgs.yarn.override(_: {
           nodejs = ourNode;
         });
+        controllerOverrides = final: prev:
+          let
+            addNativeBuildInputs = drvName: inputs: {
+              "${drvName}" = prev.${drvName}.overridePythonAttrs (
+                old: {
+                  nativeBuildInputs =
+                    (old.nativeBuildInputs or [ ]) ++ inputs;
+                }
+              );
+            };
+          in
+          { }
+          // addNativeBuildInputs "mpi4py" [ final.cython ]
+          // addNativeBuildInputs "rexfw" [ final.poetry-core ]
+          // addNativeBuildInputs "chainsail-helpers" [ final.poetry-core ];
+
+        controllerOpts = {
+          projectDir = ./app/controller;
+          overrides = poetry2nixPkg.overrides.withDefaults(controllerOverrides);
+          preferWheels = true;
+        };
+        controller = poetry2nixPkg.mkPoetryApplication controllerOpts;
+        controllerEnv = poetry2nixPkg.mkPoetryEnv controllerOpts;
+
+        basePackages = [
+          pkgs.docker-compose
+          pkgs.file
+          pkgs.gnumake
+          pkgs.kubectl
+          pkgs.kubernetes-helm
+          pkgs.minikube
+          pkgs.ncurses
+          ourNode
+          pkgs.openmpi
+          pkgs.poetry
+          pkgs.python38Packages.tkinter
+          pkgs.terraform
+          ourYarn
+        ];
+
+        pythonDevShell = ps: pkgs.mkShell {
+          packages = ps ++ basePackages;
+        };
+        controllerDevShell = pythonDevShell [
+          controllerEnv
+          controller
+        ];
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            docker-compose
-            file
-            gnumake
-            kubectl
-            kubernetes-helm
-            minikube
-            ncurses
-            ourNode
-            openmpi
-            poetry
-            python38Packages.tkinter
-            terraform
-            ourYarn
-            zlib
-          ];
-
-          # Setting the LD_LIBRARY_PATH environment variable.
-          # Can also make use of the `.overrideAttrs` method to prevent from overwriting it.
-          # See PR #310 for details: https://github.com/tweag/chainsail/pull/310.
-          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.file}/lib:${pkgs.zlib}/lib";
+        devShells = {
+          controller = controllerDevShell;
+          default = pkgs.mkShell { packages = basePackages; };
         };
         packages = {
-          controller = poetry2nixPkg.mkPoetryApplication {
-            projectDir = ./app/controller;
-            overrides = poetry2nixPkg.overrides.withDefaults (
-              final: prev:
-                let
-                  addNativeBuildInputs = drvName: inputs: {
-                    "${drvName}" = prev.${drvName}.overridePythonAttrs (
-                      old: {
-                        nativeBuildInputs =
-                          (old.nativeBuildInputs or [ ]) ++ inputs;
-                      }
-                    );
-                  };
-                in
-                { }
-                // addNativeBuildInputs "mpi4py" [ final.cython ]
-                // addNativeBuildInputs "rexfw" [ final.poetry-core ]
-            );
-          };
+          inherit controller;
           scheduler = poetry2nixPkg.mkPoetryApplication {
             projectDir = ./app/scheduler;
             overrides = poetry2nixPkg.overrides.withDefaults (
