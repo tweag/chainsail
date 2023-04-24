e81d6133863bd3ae7a8ee602fc9be5dc6fa3d2c9
Steven Shaw <steven.shaw@tweag.io>
2023-04-24 12:47:41 +1000

    Simplify run_dev_client script

--- app/client/run_dev_client.sh
+++ app/client/run_dev_client.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 SCHEDULER_URL=$(minikube service scheduler --url)
-MCMC_STATS_SERVER_URL=$(minikube service mcmc-stats-server --url)
+MCMC_STATS_URL=$(minikube service mcmc-stats-server --url)
 GRAPHITE_URL=$(minikube service graphite --url | head -n1)
 
-export SCHEDULER_URL=$SCHEDULER_URL
-export GRAPHITE_URL=$GRAPHITE_URL
-export MCMC_STATS_URL=$MCMC_STATS_SERVER_URL
+export SCHEDULER_URL
+export GRAPHITE_URL
+export MCMC_STATS_URL
 yarn run dev
