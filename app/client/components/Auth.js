57aff0074c90e9e40dd365fdb4bee87835086db2
Simeon Carstens <simeon.carstens@tweag.io>
2024-09-25 17:54:18 +0200

    Use Firebase authentication conditionally (#489)

--- app/client/components/Auth.js
+++ app/client/components/Auth.js
@@ -5,36 +5,43 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import LogRocket from 'logrocket';
 
+const { serverRuntimeConfig } = require('../next.config.js');
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   firebaseClient();
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    return firebase.auth().onIdTokenChanged(async (user) => {
-      if (!user) {
-        setUser(null);
-        nookies.set(undefined, 'token', '', {});
-        return;
-      } else {
-        try {
-          LogRocket.identify(user.uid, {
-            email: user.email,
-            name: user.displayName,
-          });
-          console.log('Identified to LogRocket');
-        } catch (e) {
-          console.log('Identify failed on ' + JSON.stringify(user) + ', session is anonyme');
-          console.log(e);
+  if (serverRuntimeConfig.require_auth) {
+    useEffect(() => {
+      return firebase.auth().onIdTokenChanged(async (user) => {
+        if (!user) {
+          setUser(null);
+          nookies.set(undefined, 'token', '', {});
+          return;
+        } else {
+          try {
+            LogRocket.identify(user.uid, {
+              email: user.email,
+              name: user.displayName,
+            });
+            console.log('Identified to LogRocket');
+          } catch (e) {
+            console.log('Identify failed on ' + JSON.stringify(user) + ', session is anonyme');
+            console.log(e);
+          }
         }
-      }
 
-      const token = await user.getIdToken();
-      setUser(user);
-      nookies.set(undefined, 'token', token, {});
+        const token = await user.getIdToken();
+        setUser(user);
+        nookies.set(undefined, 'token', token, {});
+      });
+    }, []);
+  } else {
+    useEffect(() => {
+      setUser(null);
     });
-  }, []);
+  }
   return <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>;
 };
 export const useAuth = () => useContext(AuthContext);
